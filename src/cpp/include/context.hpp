50f89d49f5bfec1c49468f7d9eff85a93786cb07
shopkeeper <734212732@qq.com>
2023-09-03 09:24:38 +0800

    update: clean up - delete message_box, mouse, text_input

--- src/cpp/include/context.hpp
+++ src/cpp/include/context.hpp
@@ -4,7 +4,6 @@
 #include "ranking_list.hpp"
 #include "renderer.hpp"
 #include "singlton.hpp"
-#include "text_input.hpp"
 #include "texture.hpp"
 #include "window.hpp"
 
@@ -12,7 +11,6 @@ class Context final : public Singlton<Context> {
    public:
     Font font;
     std::unique_ptr<TextTexture> tips;
-    TextInputHandler<10> playerIdHandler;
 
     Context(const std::string& title, const Size& windowSize);
     auto& GetRenderer() { return *renderer_; }
