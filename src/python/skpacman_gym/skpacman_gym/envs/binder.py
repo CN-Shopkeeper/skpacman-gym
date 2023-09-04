02e2ce00f4863bb6b6fd6fb62ad89de7874f3d32
shopkeeper <734212732@qq.com>
2023-09-04 11:10:24 +0800

    fix: bugs - oom by pybind11 - relative path

--- src/python/skpacman_gym/skpacman_gym/envs/binder.py
+++ src/python/skpacman_gym/skpacman_gym/envs/binder.py
@@ -1,8 +1,27 @@
 from .lib import Pacman as ctx
+from .lib import current_directory
+import numpy as np
 
 
 def get_observation():
-    return ctx.get_observation()
+    obs = ctx.get_observation()
+    pacman = obs["pacman"]
+    pacman["position"] = np.array(pacman["position"], dtype=np.float32)
+    pacman["speed"] = np.array(pacman["speed"], dtype=np.float32)
+
+    ghost1, ghost2, ghost3, ghost4 = obs["ghosts"]
+    ghost1["position"] = np.array(ghost1["position"], dtype=np.float32)
+    ghost1["speed"] = np.array(ghost1["speed"], dtype=np.float32)
+    ghost2["position"] = np.array(ghost2["position"], dtype=np.float32)
+    ghost2["speed"] = np.array(ghost2["speed"], dtype=np.float32)
+    ghost3["position"] = np.array(ghost3["position"], dtype=np.float32)
+    ghost3["speed"] = np.array(ghost3["speed"], dtype=np.float32)
+    ghost4["position"] = np.array(ghost4["position"], dtype=np.float32)
+    ghost4["speed"] = np.array(ghost4["speed"], dtype=np.float32)
+
+    obs["map_tiles"] = np.array(obs["map_tiles"])
+
+    return obs
 
 
 def get_info():
@@ -10,7 +29,7 @@ def get_info():
 
 
 def init():
-    return ctx.init()
+    return ctx.init(current_directory)
 
 
 def reset(seed=None):
